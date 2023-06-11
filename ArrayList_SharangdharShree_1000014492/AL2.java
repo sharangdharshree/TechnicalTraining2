b1fc5cda7015fd94305b94d1b974282a3a4a2d7c
Sharangdhar Shree <sharangdhar7984@gmail.com>
2023-06-11 23:42:36 +0530

    Update AL2.java

--- ArrayList_SharangdharShree_1000014492/AL2.java
+++ ArrayList_SharangdharShree_1000014492/AL2.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class AL2 {
 
+    // the i in the parameter will take the maximum/largest index of the arraylist
+    // i.e if the size of the arraylist is n then i should be n-1
+    // its work is to iterate over the arraylist
     static ArrayList<String> toUpper(ArrayList<String> arr, int i) {
         String upper;
         ArrayList<String> list = new ArrayList<String>();
